0673ed6ea0273d2a8f5e0e45e2d81748dea0f4cc
Griffin Cox <griffincox@users.noreply.github.com>
2017-01-29 17:38:28 -0600

    working on getting the proper icons to display

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,10 +6,13 @@ class GamesController < ApplicationController
     @computer_move = ["paper", "rock", "scissors"].sample
 
     if @computer_move == "rock"
+      @hand = "fa-hand-rock-o"
       @outcome = "You tied!"
     elsif @computer_move == "paper"
+      @hand = "fa-hand-paper-o"
       @outcome = "You lose!"
     else
+      @hand = "fa-hand-scissors-o"
       @outcome = "You win!"
     end
 
@@ -19,10 +22,13 @@ class GamesController < ApplicationController
     @computer_move = ["paper", "rock", "scissors"].sample
 
     if @computer_move == "scissors"
+      @hand = "fa-hand-scissors-o"
       @outcome = "You tied!"
     elsif @computer_move == "rock"
+      @hand = "fa-hand-rock-o"
       @outcome = "You lose!"
     else
+      @hand = "fa-hand-paper-o"
       @outcome = "You win!"
     end
 
@@ -32,10 +38,13 @@ class GamesController < ApplicationController
     @computer_move = ["paper", "rock", "scissors"].sample
 
     if @computer_move == "paper"
+      @hand = "fa-hand-paper-o"
       @outcome = "You tied!"
     elsif @computer_move == "scissors"
+      @hand = "fa-hand-scissors-o"
       @outcome = "You lose!"
     else
+      @hand = "fa-hand-rock-o"
       @outcome = "You win!"
     end
 
