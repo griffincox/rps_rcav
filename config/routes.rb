bfcb08db7d4d561d2b253624029ce25d8fc3ad11
Griffin Cox <griffincox@users.noreply.github.com>
2017-01-29 17:12:12 -0600

    FA, Boostrap; working on styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'games#index'
   get("/rock", { :controller => "games", :action => "play_rock" })
   get("/paper", { :controller => "games", :action => "play_paper" })
   get("/scissors", { :controller => "games", :action => "play_scissors" })
